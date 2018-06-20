5d7e6c5d9593751122e6a7e32ed5239094986e75
Raja Swathi <sr16e@my.fsu.edu>
2018-06-20 03:52:22 -0400

    Passport Facebook

--- congif.js
+++ congif.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretkey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '1285970164872946',
+        clientSecret: 'e49e847fe7664e2e3822b8d5d283af3d'
+    }
 }
